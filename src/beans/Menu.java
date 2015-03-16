5a22a77975fe5dc0cabd16115490bb72455306fd
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-03-16 15:02:14 +0100

    Initial commit

---
+++ src/beans/Menu.java
@@ -0,0 +1,36 @@
+package beans;
+
+import java.sql.Date;
+
+public class Menu {
+	
+	private int id;
+	private String nom;
+	private Date expiration;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public Date getExpiration() {
+		return expiration;
+	}
+	
+	public void setExpiration(Date expiration) {
+		this.expiration = expiration;
+	}
+	
+	
+}
